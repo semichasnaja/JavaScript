a5774abdc370a81e4b2da4d444b96f63eea4b095
semichasnaja <semichasnaja@privatbank.ua>
2022-01-28 13:46:42 +0200

    ДЗ3

--- ДЗ3/3-8-1.js
+++ ДЗ3/3-8-1.js
@@ -20,8 +20,17 @@ function f (arr) {
         }
         return result;
     }
+    f1(arr);
     return result;
 }
 
 const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
-console.log(f(arr));
+console.log(f(arr)); //12
+const arr2 = [[[[[1,2]]]]];
+console.log(f(arr2)); //3
+const arr3 = [[[[[1,2]]],2],1];
+console.log(f(arr3)); //6
+const arr4 = [[[[[]]]]];
+console.log(f(arr4)); //0
+const arr5 = [[[[[],3]]]];
+console.log(f(arr5)); //3
